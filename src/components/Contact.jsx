2951f39e71f0222641153f7f838baad32a92e622
nayetwolf <ilyas.rafiqfilali@gmail.com>
2023-12-12 20:21:16 -0500

    Pre Initial

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ilyas Filali | Contact",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "nayetwolf@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
